4cfb5de15e73288764c23b0c31061edf7923b55b
lluvianuno <lluviajnuno@gmail.com>
2025-11-08 16:40:48 -0600

    W01 Assignment: Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+
+const lastModified = new Date(document.lastModified);
+document.getElementById("lastModified").textContent =
+  "Last Modification: " + lastModified.toLocaleString();
